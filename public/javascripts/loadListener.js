96be3718f60eaeae87d5cfe59363272991a27b8a
Oleksandr Gonchar <goncharao.ukr@gmail.com>
2017-03-25 16:30:32 +0200

    update linck

--- public/javascripts/loadListener.js
+++ public/javascripts/loadListener.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", function load(event){
     console.log(event);
+    var href = window.location.href;
+    var array = href.split('/\#');
+
+    var lastsegment = array[array.length-1];
+
+    console.log(lastsegment);
 });
