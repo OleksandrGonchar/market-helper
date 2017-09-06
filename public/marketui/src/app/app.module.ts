01bafd28dc1c89a33d974fb1a2c97b6223b0c01e
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2017-09-06 17:52:47 +0300

    add skeleton for angular4 application

--- public/marketui/src/app/app.module.ts
+++ public/marketui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MarketuiComponent } from './app.component';
+import { ItemsListComponent } from './components/items-list/items-list.component';
 
 @NgModule({
   declarations: [
-    MarketuiComponent
+    MarketuiComponent,
+    ItemsListComponent
   ],
   imports: [
     BrowserModule
