de8ee0f6e6d52bb012c6b3793b96bd073e8f9add
Oleksandr Gonchar <goncharao.ukr@gmail.com>
2017-09-12 00:32:35 +0200

    minor code style refactoring

--- public/marketui/src/app/app.module.ts
+++ public/marketui/src/app/app.module.ts
@@ -1,11 +1,13 @@
+/* Base comonent */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { animate } from '@angular/animations';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* Angular material */
 import { MdToolbarModule } from '@angular/material';
-
+/* Root component */
 import { MarketuiComponent } from './app.component';
-
+/* Additional custom modules */
 import { LoginModule } from './components/login/login.module';
 import { ItemsListComponent } from './components/items-list/items-list.component';
 
