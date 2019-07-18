5781d93cdb0d81046393d0a125ddc4bd9942bed7
Oleksandr Gonchar <goncharalexandr.ukr@gmail.com>
2019-07-18 23:17:34 +0300

    try to add react

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+    
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.use('/', routes);
 app.use('/api', api);
