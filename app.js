5d3ea6f0feecc249bf5b7b45f6479b093819b318
Oleksandr Gonchar <goncharalexandr.ukr@gmail.com>
2019-07-19 00:35:27 +0300

    change feature route for react app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
     
 // Handle React routing, return all requests to React app
-app.get('/react', function(req, res) {
+app.use('/react', function(req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
